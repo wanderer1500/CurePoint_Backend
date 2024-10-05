9179e65ceab35bed5d06f80af0d10f34a5c0fca0
Avik Sarkar <aviksarkar2003sayam@gmail.com>
2024-10-05 15:16:31 +0530

    Added .env

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const MongoStore = require('connect-mongo');
 // const PORT = 8080;
+
+const dbUrl = process.env.ATLASDB_URL;
+
+async function main() {
+  try {
+    await mongoose.connect(dbUrl);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+main();
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 1 week
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+
+  },
+};
+
+app.use(session(sessionOptions));
 
 app.get("/", (req, res) => {
     res.send("Home Page");
